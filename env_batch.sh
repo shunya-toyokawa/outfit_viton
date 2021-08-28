e70b0b801bfc372c9c25f3ed79c1a22bb39760a4
shunya-toyokawa <88313532+shunya-toyokawa@users.noreply.github.com>
2021-08-28 16:07:02 +0900

    Update env_batch.sh

--- env_batch.sh
+++ env_batch.sh
@@ -2,15 +2,12 @@
 
 apt update && apt upgrade && apt install vim git curl gcc g++ curl wget 
 
-git clone https://github.com/trinanjan12/Image-Based-Virtual-Try-on-Network-from-Unpaired-Data
-
-mv Image-Based-Virtual-Try-on-Network-from-Unpaired-Data outfit_viton
 
 cd outfit_viton
 
 pip install -r requirements.txt
 
-Image-Based-Virtual-Try-on-Network-from-Unpaired-Data
+git clone --recursive https://github.com/open-mmlab/mmfashion.git
 
 cd mmfashion/
 
